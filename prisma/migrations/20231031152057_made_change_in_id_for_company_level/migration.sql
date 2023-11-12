2f35288000a07dd7a6a03c0fcd6b83c5cebbd317
Padmore Tuffour <padfor81@gmail.com>
2023-11-12 14:52:20 +0000

    fix(migration): Company level update migration fix

--- prisma/migrations/20231031152057_made_change_in_id_for_company_level/migration.sql
+++ prisma/migrations/20231031152057_made_change_in_id_for_company_level/migration.sql
@@ -1,2 +1,18 @@
+
+-- Lock Table and Related Table(s)
+LOCK TABLES
+  `_prisma_migrations` WRITE,
+  `company_level` WRITE,
+  `company_level_leave_package` WRITE;
+
+-- Disable Foreign Key checks
+SET FOREIGN_KEY_CHECKS=0;
+
 -- AlterTable
 ALTER TABLE `company_level` MODIFY `id` INTEGER UNSIGNED NOT NULL;
+
+-- Enable Foreign Key checks
+SET FOREIGN_KEY_CHECKS=1;
+
+-- Unlock Tables
+UNLOCK TABLES;
