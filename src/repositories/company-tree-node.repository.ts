2a1b810e550bd21cdc24a14c22051c3dfe32943e
Shareef-deen <sherifdeen968@gmail.com>
2024-04-29 10:56:26 +0000

    optimization: cleared commented code

--- src/repositories/company-tree-node.repository.ts
+++ src/repositories/company-tree-node.repository.ts
@@ -6,8 +6,6 @@ import {
 } from '../domain/dto/company-tree-node.dto';
 import { prisma } from '../components/db.component';
 import { AlreadyExistsError, RecordInUse } from '../errors/http-errors';
-// import { ListWithPagination, getListWithPagination } from './types';
-// import * as helpers from '../utils/helpers';
 
 export interface CreateCompanyTreeNodeObject {
   companyId: number;
@@ -16,40 +14,6 @@ export interface CreateCompanyTreeNodeObject {
   employeeId?: number;
   childNodes?: ChildNode[];
 }
-// export async function create(
-//   { jobTitleId, employeeId, companyId, parentId, ...dtoData }: CreateCompanyTreeNodeObject,
-//   includeRelations?: boolean,
-// ): Promise<CompanyTreeNodeDto> {
-//   const data: Prisma.CompanyTreeNodeCreateInput = {
-//     ...dtoData,
-//     employee: employeeId? { connect: { id: employeeId } }: undefined,
-//     jobTitle: { connect: { id: jobTitleId } },
-//     company: { connect: { id: companyId } },
-//     parent: parentId ? { connect: { id: parentId } } : undefined,
-//   };
-//   try {
-//     return await prisma.companyTreeNode.create({
-//       data,
-//       include: includeRelations
-//         ? { 
-//           parent: true, employee: true, jobTitle: true, children: {
-//             include: { jobTitle: true, employee: true } 
-//           } 
-//         }
-//         : undefined
-//     });
-//   } catch (err) {
-//     if (err instanceof Prisma.PrismaClientKnownRequestError) {
-//       if (err.code === 'P2002') {
-//         throw new AlreadyExistsError({
-//           message: 'Company tree node already exists',
-//           cause: err
-//         });
-//       }
-//     }
-//     throw err;
-//   }
-// }
 
 export async function create(
   { 
