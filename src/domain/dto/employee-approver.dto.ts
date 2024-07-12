58ab6ce8e55872827d03f2bcb5a3f319d3e97ff6
Shareef-deen <sherifdeen968@gmail.com>
2024-07-12 17:05:05 +0000

    feat: Added APIs for employee approver

---
+++ src/domain/dto/employee-approver.dto.ts
@@ -0,0 +1,43 @@
+import { Employee, EmployeeApprover } from '@prisma/client';
+import config from '../../config';
+
+export class CreateEmployeeApproverDto {
+  approverId!: number;
+  level!: number;
+}
+
+export class CreateEmployeeApproverObject {
+  employeeId!: number;
+  approverId!: number;
+  level!: number;
+}
+
+export class UpdateEmployeeApproverDto {
+  approverId?: number;
+  level?: number;
+}
+
+export enum EmployeeApproverOrderBy {
+  CREATED_AT_ASC = 'createdAt:asc',
+  CREATED_AT_DESC = 'createdAt:desc',
+  MODIFIED_AT_ASC = 'modifiedAt:asc',
+  MODIFIED_AT_DESC = 'modifiedAt:desc',
+}
+
+export class QueryEmployeeApproverDto {
+  approverId?: number;
+  level?: number;
+  inverse?: boolean;
+  page: number = 1;
+  limit: number = config.pagination.limit;
+  orderBy: EmployeeApproverOrderBy = EmployeeApproverOrderBy.CREATED_AT_DESC;
+}
+
+export class GetOneEmployeeApproverDto {
+  inverse?: boolean;
+}
+
+export interface EmployeeApproverDto extends EmployeeApprover {
+  employee?: Employee;
+  approver?: Employee;
+}
---
+++ src/repositories/employee-approver.repository.ts
@@ -0,0 +1,109 @@
+import { Prisma, } from '@prisma/client';
+import { prisma } from '../components/db.component';
+import { AlreadyExistsError, RecordInUse } from '../errors/http-errors';
+import { ListWithPagination, getListWithPagination } from './types';
+import { 
+  CreateEmployeeApproverObject, 
+  EmployeeApproverDto 
+} from '../domain/dto/employee-approver.dto';
+
+
+export async function create(
+  { employeeId, approverId, level }: CreateEmployeeApproverObject,
+  include?: Prisma.EmployeeApproverInclude,
+): Promise<EmployeeApproverDto> {
+  const data: Prisma.EmployeeApproverCreateInput = {
+    level,
+    employee: { connect: { id: employeeId } },
+    approver: { connect: { id: approverId } }
+  };
+  try {
+    return await prisma.employeeApprover.create({ data, include });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === 'P2002') {
+        throw new AlreadyExistsError({
+          message: 'Employee approver already exists',
+          cause: err
+        });
+      }
+    }
+    throw err;
+  }
+}
+
+export async function findOne(
+  whereUniqueInput: Prisma.EmployeeApproverWhereUniqueInput,
+  include?: Prisma.EmployeeApproverInclude
+): Promise<EmployeeApproverDto | null> {
+  return prisma.employeeApprover.findUnique({
+    where: whereUniqueInput,
+    include
+  });
+}
+
+export async function findFirst(
+  where: Prisma.EmployeeApproverWhereInput,
+  include?: Prisma.EmployeeApproverInclude
+): Promise<EmployeeApproverDto | null> {
+  return prisma.employeeApprover.findFirst({ where, include });
+}
+
+export async function find(params: {
+  skip?: number,
+  take?: number,
+  where?: Prisma.EmployeeApproverWhereInput,
+  orderBy?: Prisma.EmployeeApproverOrderByWithRelationAndSearchRelevanceInput,
+  include?: Prisma.EmployeeApproverInclude
+}): Promise<ListWithPagination<EmployeeApproverDto>> {
+  const { skip, take, } = params;
+  const paginate = skip !== undefined && take !== undefined;
+  const [data, totalCount] = await Promise.all([
+    prisma.employeeApprover.findMany(params),
+    paginate
+      ? prisma.employeeApprover.count({ where: params.where })
+      : Promise.resolve(undefined),
+  ]);
+
+  return getListWithPagination(data, { skip, take, totalCount });
+}
+
+export async function update(params: {
+  where: Prisma.EmployeeApproverWhereUniqueInput,
+  data: Prisma.EmployeeApproverUpdateInput,
+  include?: Prisma.EmployeeApproverInclude
+}): Promise<EmployeeApproverDto> {
+  try {
+    return await prisma.employeeApprover.update(params);
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === 'P2002') {
+        throw new AlreadyExistsError({
+          message: 'Employee approver already exists',
+          cause: err
+        });
+      }
+    }
+    throw err;
+  }
+}
+
+export async function deleteOne(
+  whereUniqueInput: Prisma.EmployeeApproverWhereUniqueInput
+): Promise<EmployeeApproverDto> {
+  try {
+    return await prisma.employeeApprover.delete({
+      where: whereUniqueInput
+    });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === 'P2003') {
+        throw new RecordInUse({
+          message: 'Employee approver is currently in use',
+          cause: err
+        });
+      }
+    }
+    throw err;
+  }
+}
